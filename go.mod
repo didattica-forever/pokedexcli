55421d018a890228c59b0ed2d6aaff14120f0516
Paolo M. <didattica@gmx.com>
2025-09-05 05:13:19 +0200

    First Load

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/didattica-forever/pokedexcli
+
+go 1.24.6
---
+++ internal/pokeapi/client.go
@@ -0,0 +1,25 @@
+package pokeapi
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/didattica-forever/pokedexcli/internal/pokecache"
+)
+
+// Client -
+type Client struct {
+	cache      pokecache.Cache
+	httpClient http.Client
+}
+
+// NewClient -
+func NewClient(timeout, cacheInterval time.Duration) Client {
+	return Client{
+		cache: pokecache.NewCache(cacheInterval),
+		httpClient: http.Client{
+			Timeout: timeout,
+		},
+	}
+}
+
